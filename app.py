64a26f8b7cfd416f2afe866914573df147724eb1
cap-ma <uone2323@gmail.com>
2022-09-12 19:59:30 +0500

    all fixed

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 
+from pathlib import Path
 from aiogram import types,Dispatcher,Bot,executor
 from aiogram.dispatcher.filters import Text
+from aiogram.utils.executor import start_webhook
 from loader import instagramdownloader,tiktok_downloader, youtubedownloader
 
 import json
@@ -8,6 +10,15 @@ import uuid
 API_TOKEN='5796481105:AAFk80b4qWB3ChrGpUTB0EU4tmITcrLP6c8'
 bot=Bot(token=API_TOKEN)
 dp=Dispatcher(bot)
+HEROKU_APP_NAME='downloadertiyuin'
+HOST=f'https://downloadertiyuin.herokuapp.com/'
+PATH=f'/webhook/{API_TOKEN}'
+URL=f'{HOST}{PATH}'
+WEB_HOST='0.0.0.0'
+PORT=8000
+
+
+
 
 
 @dp.message_handler(commands=['start','/help'])
@@ -58,10 +69,24 @@ async def download_youtube_video(messege:types.Message):
         print(" smth with the meomry of the video bad happened")
 
     
-    
+async def on_startup(dispatcher):
+    await bot.set_webhook(URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
 
 
 
 if __name__=='__main__':
-    executor.start_polling(dp,skip_updates=True)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=HOST,
+        port=PORT,
+    )
+    
 
